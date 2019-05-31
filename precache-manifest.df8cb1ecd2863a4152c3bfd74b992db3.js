da3ec37797173cccbe87144438d3f661d128cd77
Viren Talwar <virentalwar@Virens-MacBook-Pro.local>
2019-05-31 21:39:31 +0530

    Updates

---
+++ precache-manifest.df8cb1ecd2863a4152c3bfd74b992db3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/revise/static/media/reactlogo.47ce6e77.png"
+  },
+  {
+    "revision": "08c4dabf346004a6ae40",
+    "url": "/revise/static/js/runtime~main.2a05bc75.js"
+  },
+  {
+    "revision": "7eab65388c449c07f3b0",
+    "url": "/revise/static/js/main.80aac5c4.chunk.js"
+  },
+  {
+    "revision": "ec663e167d191ceb77c1",
+    "url": "/revise/static/js/2.7ba089e1.chunk.js"
+  },
+  {
+    "revision": "87f56bfb85fd61b8ddf399425597dcbe",
+    "url": "/revise/index.html"
+  }
+];
